ec6a4c8d98e308b21b772315d703acd5509e34f4
Jonathan Lopes Machado <jonathan.lopes1004@gmail.com>
2022-04-24 14:35:22 -0300

    completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a>
+          <Image src="/logo.svg" width={250} height={100} alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
